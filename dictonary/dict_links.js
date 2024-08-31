db2e17f6e91458ccada2993db3f3fbad64d2586e
turtleheart <nesp212212@gmail.com>
2024-08-31 11:40:09 +0200

    fixes link

--- dictonary/dict_links.js
+++ dictonary/dict_links.js
@@ -1,7 +1,7 @@
 export const dictLinks = {
     "EN(UK)" : {
         "dnd-create" : "DND/Character_Creation.pdf",
-        "dnd-downtime" : "DND/DowntimeActivities-player-version.pdf",
+        "dnd-downtime" : "DND/DowntimeActivities.pdf",
         "dnd-dungeonExploration" : "DND/Dungeon_Exploration.pdf",
         "dnd-campaign-socialEncounters" : "DND/Social_Encounters.pdf",
         "dnd-depCampaign-strongholds" : "DND/Strongholds.pdf",
